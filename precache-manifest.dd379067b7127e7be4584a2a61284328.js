31a77e4f2a027c71c30d165d428b5c8fd390132a
Chai Mun Kiat <munkiat08@gmail.com>
2020-05-22 17:49:34 +0800

    rename repo

---
+++ precache-manifest.dd379067b7127e7be4584a2a61284328.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a2e12f63a80007bbb935971ca82b68",
+    "url": "/munkiat/index.html"
+  },
+  {
+    "revision": "4cfe2f6fd2e17d766112",
+    "url": "/munkiat/static/css/main.e9ae420d.chunk.css"
+  },
+  {
+    "revision": "b8e07f00d8736924017c",
+    "url": "/munkiat/static/js/2.b379e365.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/munkiat/static/js/2.b379e365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cfe2f6fd2e17d766112",
+    "url": "/munkiat/static/js/main.bf7c74b4.chunk.js"
+  },
+  {
+    "revision": "86a0ee7fd788ecd700d3",
+    "url": "/munkiat/static/js/runtime-main.db87466e.js"
+  },
+  {
+    "revision": "644f9294f360f3a3ed8622da6f9a2d3b",
+    "url": "/munkiat/static/media/bg.644f9294.jpg"
+  },
+  {
+    "revision": "fd9c7b361f3d665a17fbcd399c5799bc",
+    "url": "/munkiat/static/media/profile.fd9c7b36.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.4f0e4b40bb8620f17270ee94b4c0c84e.js"
+  "/munkiat/precache-manifest.dd379067b7127e7be4584a2a61284328.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/munkiat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
