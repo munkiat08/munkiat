493acf5684fd76906c8b639f2f2982604d6baffa
Chai Mun Kiat <munkiat08@gmail.com>
2020-05-22 15:41:29 +0800

    gh version

---
+++ precache-manifest.7e90fb4ac7d748850f14fcec419bc08f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c4e8229bfd85aa29725dc94a1d38bd",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "20f20644ceaae04bb99c",
+    "url": "/resume/static/css/main.6aadd557.chunk.css"
+  },
+  {
+    "revision": "b8e07f00d8736924017c",
+    "url": "/resume/static/js/2.b379e365.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/resume/static/js/2.b379e365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20f20644ceaae04bb99c",
+    "url": "/resume/static/js/main.8d6ad606.chunk.js"
+  },
+  {
+    "revision": "e0f7dc91a34c7427a62d",
+    "url": "/resume/static/js/runtime-main.a54ac2ac.js"
+  },
+  {
+    "revision": "fd9c7b361f3d665a17fbcd399c5799bc",
+    "url": "/resume/static/media/profile.fd9c7b36.jpg"
+  }
+]);
