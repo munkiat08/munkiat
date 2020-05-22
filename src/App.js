c18225bbb0a420e4bf71157f51a98e6aad3e5ad4
Chai Mun Kiat <munkiat08@gmail.com>
2020-05-22 15:33:09 +0800

    build

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
 
 import LinkButton from './component/LinkButton';
